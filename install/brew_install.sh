c83310e7d6256f97a8a7f06cb8918ad24f9492ea
暮晨 <ducheng@360.net>
2018-04-19 23:18:44 +0800

    改成可直接运行的脚本

---
+++ install/brew_install.sh
@@ -0,0 +1,16 @@
+brew install vim --with-override-vi
+brew install zsh
+brew install wget
+brew install autojump
+brew install git
+brew install tig
+brew install ctags
+brew install fzf
+brew install ag
+brew install go
+brew install tmux
+brew install tmate
+brew install redis
+brew install reattach-to-user-namespace
+brew install nmap
+brew install sqlmap
