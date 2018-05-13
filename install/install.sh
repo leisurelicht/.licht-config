e95b19b3aad2ac1c7548568d29242e0e79309fd5
暮晨 <ducheng@360.net>
2018-05-13 23:34:36 +0800

    update

--- install/install.sh
+++ install/install.sh
@@ -1,21 +1,25 @@
-basepath=$(
-  cd $(dirname $0)/..
+install_path=$(
+  cd $(dirname $0)
   pwd
 )
-echo "配置文件根目录为: ${basepath}"
+config_path=$(
+  cd $install_path/..
+  pwd
+)
+echo "====> Config file root path is: ${config_path}"
 
 if ! command -v brew >/dev/null 2>&1; then
-    echo "brew is not be installed, start to install"
+    echo "====> Command brew is not be installed, start to install"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    echo "brew is already install"
+    echo "====> Commnad brew is already install"
 fi
 
-echo "update brew and run doctor"
+echo "====> Update brew and run doctor"
 brew update
 brew doctor
 
-echo "use brew to intall necessary"
+echo "====> Use brew to intall necessary"
 brew install zsh
 brew install vim --with-override-vi
 brew install node
@@ -25,6 +29,65 @@ brew install ctags
 brew install ag
 brew install tmux
 brew install autojump
+
+# 切换shell
+echo "====> Change shell"
+chsh -s /bin/zsh
+
+# 安装oh-my-zsh
+echo "====> Install oh-my-zsh"
+if command -v curl >/dev/null 2>&1; then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+elif command -v wget >/dev/null 2>&1; then
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+# tmux
+echo "====> Install tumx plugins manage plugin tpm"
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    mkdir -p ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# vim-plug
+echo "====> Install vim plugins manage plugin vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "====> Create back up dir"
+mkdir $conf_path/bak
+
+if [ -e ~/.zshrc ]; then
+    echo "====> Zsh config file zshrc is exist, backup and delete it."
+    mv ~/.zshrc $basepath/bak/
+fi
+echo "====> Create zshrc link"
+ln -s $basepath/zsh/zshrc ~/.zshrc
+
+if [ -e ~/.vimrc ]; then
+    echo "====> Vim config file vimrc is exist, backup and delete it."
+    mv ~/.vimrc $basepath/bak/
+fi
+echo "====> Create vimrc link"
+ln -s $basepath/vim/vimrc ~/.vimrc
+
+if [ -e ~/.tmux.conf ]; then
+    echo "====> Tmux config file tmux.confis exist, backup and delete it."
+    mv ~/.tmux.conf $basepath/bak/
+fi
+echo "====> Create tmux.conf link"
+ln -s $basepath/tmux/tmux.conf ~/.tmux.conf
+
+if [ ! -d ~/.config/ ]; then
+    mkdir ~/.config/
+fi
+
+if [ -d ~/.config/nvim ]; then
+    echo "====> Neovim config dir nvim is exist, backup and delete it."
+    mv ~/.config/nvim $basepath/bak/
+fi
+echo "====> Create neovim config file links"
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 function install_from_file(){
     while read -r line
@@ -37,85 +100,39 @@ function install_from_file(){
 
 for parm in $@; do
     if [[ param == "all" ]]; then
-        install_from_file ./brew_install brew install
-        install_from_file ./brew_cask_install brew install
+        echo "====> Use brew and brew cask install software"
+        install_from_file $install_path/brew_install brew install
+        install_from_file $install_path/brew_cask_install brew install
     fi
 
     if [[ param == "brew" ]];then
-        echo "开始用brew 安装命令行软件。"
-        install_from_file ./brew_install brew install
+        echo "====> Use brew to install command"
+        install_from_file $install_path/brew_install brew install
     fi
 
     if [[ param == "brewcask" ]]; then
-        echo "开始用brew cask 安装图形界面软件。"
-        install_from_file ./brew_cask_install brew cask install
+        echo "====> Use brew cask to install software"
+        install_from_file $install_path/brew_cask_install brew cask install
     fi
 done
 
-echo "开始用Python 包管理器pip 安装第三方包。"
-install_from_file ./pip_install /usr/local/bin/pip3 install
+echo "====> Use pip to install package"
+install_from_file $install_path/pip_install /usr/local/bin/pip3 install
 
 if command -v brew >/dev/null 2>&1; then
-    install_from_file ./pip3_install /usr/local/bin/pip install
+    install_from_file $install_path/pip3_install /usr/local/bin/pip install
 fi
 
-echo "开始用Ruby 包管理器gem 安装第三方包。"
-install_from_file ./gem_install gem install
+echo "====> Use gem to install package"
+install_from_file $install_path/gem_install gem install
 
-echo "开始用node 包管理器npm 安装第三方包。" install_from_file ./npm_install npm install -g
+echo "====> Use npm to install package" 
+install_from_file $install_path/npm_install npm install -g
 
-echo "开始用Go 包管理器安装第三方包。"
-install_from_file ./go_install go get -u
-
-mkdir $basepath/bak
-
-if [ -e ~/.zshrc ]; then
-    echo "zshrc file is exist, backup and delete it."
-    mv ~/.zshrc $basepath/bak/
-fi
-ln -s $basepath/zsh/zshrc ~/.zshrc
-
-if [ -e ~/.vimrc ]; then
-    echo "vimrc file is exist, backup and delete it."
-    mv ~/.vimrc $basepath/bak/
-fi
-ln -s $basepath/vim/vimrc ~/.vimrc
-
-if [ -e ~/.tmux.conf ]; then
-    echo "tmux.conf file is exist, backup and delete it."
-    mv ~/.tmux.conf $basepath/bak/
-fi
-ln -s $basepath/tmux/tmux.conf ~/.tmux.conf
-
-if [ ! -d ~/.config/ ]; then
-    mkdir ~/.config/
-fi
-
-if [ -d ~/.config/nvim ]; then
-    echo "nvim dir is exist, backup and delete it."
-    mv ~/.config/nvim $basepath/bak/
-fi
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-
-# 切换shell
-chsh -s /bin/zsh
-
-# 安装oh-my-zsh
-if command -v curl >/dev/null 2>&1; then
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-elif command -v wget >/dev/null 2>&1; then
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-fi
-
-# tmux
-if [ ! -d ~/.tmux/plugins/tpm ]; then
-    mkdir -p ~/.tmux/plugins/tpm
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
+echo "====> Use go to install package"
+install_from_file $install_path/go_install go get -u
 
 # 安装vim插件
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "====> Install vim PlugInstall"
 vim -c PlugInstall
 
