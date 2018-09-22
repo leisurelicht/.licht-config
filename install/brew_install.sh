dd95e220733c03cf936f6602112e2806e3f2c241
暮晨 <362864550@qq.com>
2018-09-22 21:25:57 +0800

    记录安装的软件

---
+++ install/brew_install.sh
@@ -0,0 +1,12 @@
+brew install mycli
+brew install wget
+brew install git
+brew install tig
+brew install tmate
+brew install redis
+brew install nmap
+brew install sqlmap
+brew install cloc
+brew install ctop
+brew install lnav
+brew install gibo
