8d9741ef9910e9623af5fe37394b6c596e423332
暮晨 <leisurelicht@gmail.com>
2022-03-30 23:50:03 +0800

    refactor: 💡 调整代码

--- vi/nvim/lua/lsp/nvim-cmp.lua
+++ vi/nvim/lua/lsp/nvim-cmp.lua
@@ -60,7 +60,7 @@ cmp.setup.cmdline(':', {
   sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
 })
 
-local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol
-                                                          .make_client_capabilities())
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return capabilities
