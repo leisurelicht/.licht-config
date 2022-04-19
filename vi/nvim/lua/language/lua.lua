33bd52585470122302dee2f4272bf30fef313fab
暮晨 <leisurelicht@gmail.com>
2022-04-19 23:43:38 +0800

    feat: 🎸 close lua lsp code foramt

--- vi/nvim/lua/language/lua.lua
+++ vi/nvim/lua/language/lua.lua
@@ -31,16 +31,12 @@ return {
 
   on_attach = function(client, bufnr)
     -- 关闭lsp 的自动格式化
-    -- client.resolved_capabilities.document_formatting = false
-    -- client.resolved_capabilities.document_range_formatting = false
-
-    -- print("client:"..vim.inspect(client))
-    -- print("bufnr:"..bufnr)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
 
     require('lsp.keybindings').register(client, bufnr)
 
     -- 保存时使用lsp的自动格式化
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    -- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
   end
-
 }
