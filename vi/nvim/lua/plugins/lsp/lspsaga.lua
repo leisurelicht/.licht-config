160c8fd0fa6552bead29ee82ed2b2c1586b74f55
暮晨 <leisurelicht@gmail.com>
2022-05-07 01:06:14 +0800

    feat: 🎸 add autocmd

--- vi/nvim/lua/plugins/lsp/lspsaga.lua
+++ vi/nvim/lua/plugins/lsp/lspsaga.lua
@@ -43,3 +43,12 @@ lspsaga.setup {
     exec = "<CR>"
   }
 }
+
+vim.cmd(
+  [[
+augroup lspsaga_filetypes
+  autocmd!
+  autocmd FileType LspsagaHover nnoremap <buffer><nowait><silent> <Esc> <cmd>close!<cr>
+augroup END
+]]
+)
