4efc1393ec80afaeb289044df586c1e781433f8e
暮晨 <leisurelicht@gmail.com>
2022-05-22 22:52:22 +0800

    feat: 🎸 update cmp keybinding

--- vi/nvim/lua/plugins/lsp_cmp.lua
+++ vi/nvim/lua/plugins/lsp_cmp.lua
@@ -14,10 +14,15 @@ if not lspkind_ok then
 	vim.notify("Load lspkind Failed", "warn")
 	return
 end
-local cmp_nvim_lsp_ok, _ = pcall(require, "cmp_nvim_lsp")
-if not cmp_nvim_lsp_ok then
-	vim.notify("Load cmp_nvim_lsp Failed", "warn")
-	return
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
 cmp.setup({
@@ -31,10 +36,29 @@ cmp.setup({
 		["<c-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "s", "c" }),
 		["<c-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s", "c" }),
 		["<c-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s", "c" }),
-		["<TAB>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s", "c" }),
-		["<CR>"] = cmp.mapping(cmp.mapping.confirm(), { "i", "s", "c" }),
+		-- ["<TAB>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s", "c" }),
+		-- ["<CR>"] = cmp.mapping(cmp.mapping.confirm(), { "i", "s", "c" }),
 		["<c-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "s", "c" }),
 		["<c-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "s", "c" }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+			end
+		end, { "i", "s", "c"}),
+		["<S-Tab>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s", "c"}),
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
