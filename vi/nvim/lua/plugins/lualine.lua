a10860acf877046e78785a9cef555663ec1b03fc
暮晨 <leisurelicht@gmail.com>
2022-05-28 11:45:17 +0800

    feat: 🎸 update

--- vi/nvim/lua/plugins/lualine.lua
+++ vi/nvim/lua/plugins/lualine.lua
@@ -9,16 +9,24 @@ if not ok then
 	return
 end
 
+local icons_d = require("utils.icons").diagnostics
+
 local function window_num()
 	-- local num = vim.inspect([[%{tabpagewinnr(tabpagenr())}]])
 	local num = [[%{winnr()}]]
 	return "[" .. num .. "]"
 end
 
+
+
 lualine.setup({
 	options = { theme = "catppuccin" },
 	sections = {
 		lualine_a = {
+			{
+				"tabs",
+				separator = { right = "" },
+			},
 			{
 				window_num,
 				separator = { right = "" },
@@ -32,6 +40,36 @@ lualine.setup({
 			},
 		},
 		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = {
+            {
+				"diagnostics",
+				-- Table of diagnostic sources, available sources are:
+				--   'nvim_lsp', 'nvim_diagnostic', 'coc', 'ale', 'vim_lsp'.
+				-- or a function that returns a table as such:
+				--   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+				sources = { "nvim_diagnostic", "nvim_lsp" },
+
+				-- Displays diagnostics for the defined severity types
+				sections = { "error", "warn", "info", "hint" },
+
+				diagnostics_color = {
+					-- Same values as the general color option can be used here.
+					error = "DiagnosticError", -- Changes diagnostics' error color.
+					warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
+					info = "DiagnosticInfo", -- Changes diagnostics' info color.
+					hint = "DiagnosticHint", -- Changes diagnostics' hint color.
+				},
+				symbols = { error = icons_d.Error, warn = icons_d.Warn, info = icons_d.Info, hint = icons_d.Hint },
+				colored = true, -- Displays diagnostics status in color if set to true.
+				update_in_insert = false, -- Update diagnostics in insert mode.
+				always_visible = false, -- Show diagnostics even if there are none.
+			},
+            {
+                "progress",
+                separator = { left = "" }
+            }
+        }
 	},
 	inactive_sections = {
 		lualine_a = {
