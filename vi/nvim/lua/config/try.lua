1509e1bb6372b71a818e9038ac536482b0474b38
暮晨 <leisurelicht@gmail.com>
2023-05-30 22:30:09 +0800

    feat: add try

---
+++ vi/nvim/lua/config/try.lua
@@ -0,0 +1,32 @@
+-- =================
+-- lazy.lua
+-- Note: plugins manage lazy config
+-- Link: https://github.com/folke/lazy.nvim
+-- =================
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "leisurelicht/LichtVim", import = "lichtvim.plugins" },
+
+    { import = "lichtvim.plugins.extras.lang.go" },
+    { import = "lichtvim.plugins.extras.copilot.attach" },
+  },
+  dev = { path = "~/Code/neovim" },
+  defaults = { lazy = false, version = false },
+  ui = { border = "rounded", title = " Lazy Plugins Manager " },
+  install = { missing = true, colorscheme = { "tokyonight", "vscode" } },
+  checker = { enabled = true },
+})
