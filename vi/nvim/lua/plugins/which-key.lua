4a72cafc9e190a8fcbfbb559992771485aae71bd
暮晨 <leisurelicht@gmail.com>
2023-08-30 14:45:30 +0800

    feat: update which-key config

--- vi/nvim/lua/plugins/which-key.lua
+++ vi/nvim/lua/plugins/which-key.lua
@@ -2,6 +2,10 @@ return {
   {
     "folke/which-key.nvim",
     opts = {
+      icons = {
+        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+        separator = "", -- symbol used between a key and it's label
+      },
       window = {
         border = "rounded", -- none, single, double, shadow
         position = "bottom", -- bottom, top
