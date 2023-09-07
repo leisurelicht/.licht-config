2155aa82cf74d3a402c35682c36a300151c44628
leisurelicht <leisurelicht@gmail.com>
2023-09-08 01:27:07 +0800

    feat: add treesitter config

---
+++ vi/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    dependencies = {
+      "RRethy/nvim-treesitter-endwise",
+    },
+    keys = {
+      { "<leader>pT", "<cmd>TSModuleInfo<cr>", desc = "Treesitter info" },
+    },
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+      })
+      opts.matchup = { enable = true }
+      opts.endwise = { enable = true }
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.install").prefer_git = true
+    end,
+  },
+}
