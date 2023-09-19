5ee097b1633eb12940383ccbf12fe98491717484
暮晨 <leisurelicht@gmail.com>
2023-09-19 18:00:14 +0800

    refactor: 修改文件结构

--- vi/nvim/lua/plugins/editor/init.lua
+++ vi/nvim/lua/plugins/editor/init.lua
@@ -1,4 +1,5 @@
 local map = require("utils.map")
+local Util = require("lazy.core.util")
 
 return {
   { import = "plugins.editor.treesitter" },
@@ -51,5 +52,64 @@ return {
       { "<leader>uo", "<cmd>ColorizerToggle<cr>", desc = "Toggle Colorizer" },
     },
     config = true,
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    event = "VeryLazy",
+    opts = {
+      ignored_filetypes = { "nofile", "quickfix", "prompt", "alpha", "neo-tree", "toggleterm" },
+      ignored_buftypes = { "nofile", "NvimTree", "terminal" },
+      move_cursor_same_row = false,
+      resize_mode = {
+        silent = true,
+        hooks = {
+          on_enter = function()
+            Util.info("Entering Resize Mode. Welcome")
+          end,
+          on_leave = function()
+            Util.info("Exiting Resize Mode. Bye")
+          end,
+        },
+      },
+    },
+    config = function(_, opts)
+      require("smart-splits").setup(opts)
+      map.lazy("n", "<leader>us", function()
+        require("smart-splits").start_resize_mode()
+      end, { desc = "Enable resize mode" })
+      map.lazy("n", "<leader>uS", "<cmd>tabdo wincmd =<cr>", { desc = "Resume size" })
+    end,
+  },
+  {
+    "brglng/vim-im-select",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>ui",
+        function()
+          vim.cmd([[ImSelectEnable]])
+          Util.info("Enabled im select")
+        end,
+        desc = "Enable imselect",
+      },
+      {
+        "<leader>uI",
+        function()
+          vim.cmd([[ImSelectDisable]])
+          Util.warn("Disabled im select")
+        end,
+        desc = "Disable imselect",
+      },
+    },
+    init = function()
+      vim.g.im_select_enable_focus_events = 1
+      if utils.sys.is_macos() or utils.sys.is_linux() then
+        vim.api.nvim_create_autocmd({ "TermEnter" }, {
+          group = vim.api.nvim_create_augroup("im_select", { clear = true }),
+          pattern = { "*" },
+          command = "call system('im-select com.apple.keylayout.ABC')",
+        })
+      end
+    end,
   },
 }
