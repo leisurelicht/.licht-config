4304a5388042c9686798958cf9354b8aeb270517
暮晨 <leisurelicht@gmail.com>
2023-10-09 11:53:17 +0800

    feat: update map

--- vi/nvim/lua/utils/map.lua
+++ vi/nvim/lua/utils/map.lua
@@ -7,17 +7,9 @@
 local M = {}
 
 function M.set(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    if opts.remap and not vim.g.vscode then
-      opts.remap = nil
-    end
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 function M.del(mode, lhs, opts)
