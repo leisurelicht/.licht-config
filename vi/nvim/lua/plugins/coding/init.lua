fd16110be1a418d648d927b7cf1177dfa3166c3c
暮晨 <leisurelicht@gmail.com>
2023-10-13 15:32:51 +0800

    fix: lsp keymap error

--- vi/nvim/lua/plugins/coding/init.lua
+++ vi/nvim/lua/plugins/coding/init.lua
@@ -2,7 +2,7 @@ return {
   { import = "plugins.coding.gitsigns" },
   {
     "neovim/nvim-lspconfig",
-    init = function()
+    keys = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- change a keymap
       keys[#keys + 1] = { "<leader>lI", "<cmd>LspInfo<cr>", desc = "Show LSP Info" }
