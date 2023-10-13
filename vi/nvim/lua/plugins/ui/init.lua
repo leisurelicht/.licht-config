19a7e04c5c59b213effd1220ad10a34d02361695
暮晨 <leisurelicht@gmail.com>
2023-10-13 15:08:09 +0800

    feat: delete alpha

--- vi/nvim/lua/plugins/ui/init.lua
+++ vi/nvim/lua/plugins/ui/init.lua
@@ -4,26 +4,6 @@ return {
   { import = "plugins.ui.lualine" },
   { import = "plugins.ui.fold" },
   {
-    "goolord/alpha-nvim",
-    optional = true,
-    opts = function(_, opts)
-      local dashboard = require("alpha.themes.dashboard")
-      opts.section.buttons.val = {
-        dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("o", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-      }
-
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-    end,
-  },
-  {
     "folke/noice.nvim",
     optional = true,
     keys = {
@@ -32,11 +12,7 @@ return {
       { "<leader>sna", false },
       { "<leader>snd", false },
     },
-    opts = {
-      presets = {
-        lsp_doc_border = true,
-      },
-    },
+    opts = { presets = { lsp_doc_border = true } },
   },
   {
     "m4xshen/smartcolumn.nvim",
