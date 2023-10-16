2db3b267197544df8ff756f68ce2f97c514abf7c
暮晨 <leisurelicht@gmail.com>
2023-10-16 12:33:26 +0800

    feat: 🎸 update

--- vi/nvim/lua/config/plugs.lua
+++ vi/nvim/lua/config/plugs.lua
@@ -42,9 +42,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.navic" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
 
