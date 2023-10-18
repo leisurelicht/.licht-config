8f34c9023979ce835de00ca9e135d24572d2839f
暮晨 <leisurelicht@gmail.com>
2023-10-18 10:44:01 +0800

    feat: update

--- vi/nvim/lua/plugins/ui/fold.lua
+++ vi/nvim/lua/plugins/ui/fold.lua
@@ -5,7 +5,7 @@ return {
     optional = true,
     opts = {
       signs = {
-        add = { text = "┋" },
+        add = { text = "┃" },
         change = { text = "┃" },
         delete = { text = "󱈸" },
         topdelete = { text = "󱈸" },
