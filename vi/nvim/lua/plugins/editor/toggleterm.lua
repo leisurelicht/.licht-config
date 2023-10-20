3b1159c461a3b042cda6e5a1685f95d01a5df5c8
leisurelicht <leisurelicht@gmail.com>
2023-10-21 02:27:00 +0800

    refactor: toggleterm config

--- vi/nvim/lua/plugins/editor/toggleterm.lua
+++ vi/nvim/lua/plugins/editor/toggleterm.lua
@@ -1,27 +1,63 @@
 local Util = require("lazyvim.util")
 local map = require("utils.map")
 
+--- Add a terminal window
+local function smart_add_terminal()
+  ---@diagnostic disable-next-line: undefined-field
+  if vim.b.toggle_number == nil then
+    Util.warn("Need to create a terminal and move in it first", { title = " ToggleTerm" })
+    return
+  end
+
+  local direction = require("toggleterm.ui").guess_direction()
+
+  if direction == nil then
+    if vim.g._term_direction == 1 then
+      direction = "vertical"
+    elseif vim.g._term_direction == 2 then
+      direction = "horizontal"
+    elseif vim.g._term_direction == 0 then
+      Util.warn("Can not add a terminal window", { title = " ToggleTerm" })
+      return
+    end
+  end
+
+  Util.info("direction: " .. direction)
+
+  -- Util.info("direction: " .. vim.g._term_direction)
+
+  if direction == "vertical" then
+    vim.g._term_direction = 1
+    vim.cmd("exe b:toggle_number+1.'ToggleTerm direction=vertical'")
+  elseif direction == "horizontal" then
+    vim.g._term_direction = 2
+    vim.cmd("exe b:toggle_number+1.'ToggleTerm direction=horizontal'")
+  elseif direction == "float" then
+    Util.info("Float terminal can not be copy")
+  end
+end
+
 return {
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.defaults["<leader>o"] = { name = " Terminal" }
+    end,
+  },
   {
     "akinsho/toggleterm.nvim",
     cmd = "ToggleTerm",
-    keys = function()
-      local has_which, which_key = pcall(require, "which-key")
-      if has_which then
-        which_key.register({ mode = { "n" }, ["<leader>o"] = { name = " Terminal" } })
-      end
-
-      return {
-        { "<C-\\>", "<CMD>exe v:count1 . 'ToggleTerm'<CR>", desc = "Toggle terminal" },
-        { "<leader>of", "<CMD>ToggleTerm direction=float<CR>", desc = "Toggle in float" },
-        { "<leader>ot", "<CMD>ToggleTerm direction=tab<CR>", desc = "Toggle in tab" },
-        { "<leader>oh", "<CMD>ToggleTerm direction=horizontal<CR>", desc = "Toggle in horizontal" },
-        { "<leader>ov", "<CMD>ToggleTerm direction=vertical<CR>", desc = "Toggle in vertical" },
-        { "<leader>or", "<CMD>ToggleTermSendCurrentLine<CR>", desc = "Send current line" },
-        { "<leader>or", "<CMD>ToggleTermSendVisualLines<CR>", desc = "Send visual lines" },
-        { "<leader>os", "<CMD>ToggleTermSendVisualSelection<CR>", desc = "Send visual selection" },
-      }
-    end,
+    keys = {
+      { "<C-\\>", "<CMD>exe v:count1 . 'ToggleTerm'<CR>", desc = "Toggle terminal" },
+      { "<leader>of", "<CMD>ToggleTerm direction=float<CR>", desc = "Toggle in float" },
+      { "<leader>ot", "<CMD>ToggleTerm direction=tab<CR>", desc = "Toggle in tab" },
+      { "<leader>oh", "<CMD>ToggleTerm direction=horizontal<CR>", desc = "Toggle in horizontal" },
+      { "<leader>ov", "<CMD>ToggleTerm direction=vertical<CR>", desc = "Toggle in vertical" },
+      { "<leader>or", "<CMD>ToggleTermSendCurrentLine<CR>", desc = "Send current line" },
+      { "<leader>or", "<CMD>ToggleTermSendVisualLines<CR>", desc = "Send visual lines" },
+      { "<leader>os", "<CMD>ToggleTermSendVisualSelection<CR>", desc = "Send visual selection" },
+    },
     opts = {
       size = function(term)
         if term.direction == "horizontal" then
@@ -52,42 +88,6 @@ return {
     end,
     config = function(_, opts)
       require("toggleterm").setup(opts)
-
-      --- Add a terminal window
-      local function smart_add_terminal()
-        ---@diagnostic disable-next-line: undefined-field
-        if vim.b.toggle_number == nil then
-          Util.warn("Need to create a terminal and move in it first", { title = " ToggleTerm" })
-          return
-        end
-
-        local direction = require("toggleterm.ui").guess_direction()
-
-        if direction == nil then
-          if vim.g._term_direction == 1 then
-            direction = "vertical"
-          elseif vim.g._term_direction == 2 then
-            direction = "horizontal"
-          elseif vim.g._term_direction == 0 then
-            Util.warn("Can not add a terminal window", { title = " ToggleTerm" })
-            return
-          end
-        end
-
-        Util.info("direction: " .. direction)
-
-        -- Util.info("direction: " .. vim.g._term_direction)
-
-        if direction == "vertical" then
-          vim.g._term_direction = 1
-          vim.cmd("exe b:toggle_number+1.'ToggleTerm direction=vertical'")
-        elseif direction == "horizontal" then
-          vim.g._term_direction = 2
-          vim.cmd("exe b:toggle_number+1.'ToggleTerm direction=horizontal'")
-        elseif direction == "float" then
-          Util.info("Float terminal can not be copy")
-        end
-      end
 
       vim.api.nvim_create_autocmd({ "TermOpen" }, {
         group = vim.api.nvim_create_augroup("toggleterm_keymap", { clear = true }),
