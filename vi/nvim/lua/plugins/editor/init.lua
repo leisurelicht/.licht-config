4c7d222640cb10199c2570ca82f9fbdcaea6094d
暮晨 <leisurelicht@gmail.com>
2023-10-20 15:58:42 +0800

    refactor: neo-tree config

--- vi/nvim/lua/plugins/editor/init.lua
+++ vi/nvim/lua/plugins/editor/init.lua
@@ -112,4 +112,45 @@ return {
       end
     end,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    dependencies = {
+      {
+        "s1n7ax/nvim-window-picker",
+        version = "v2.*",
+        event = "VeryLazy",
+        opts = {
+          hint = "floating-big-letter",
+          selection_chars = "ABFJDKSL;CMRUEIWOQP",
+          show_prompt = false,
+          filter_rules = {
+            bo = {
+              filetype = { "neo-tree", "neo-tree-popup", "notify", "noice" },
+              buftype = { "terminal", "quickfix" },
+            },
+          },
+        },
+      },
+    },
+    opts = {
+      close_if_last_window = true,
+      source_selector = {
+        winbar = true,
+        content_layout = "center",
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+          ["S"] = "none",
+          -- ["m"] = "none",
+          ["o"] = "split_with_window_picker",
+          ["s"] = "vsplit_with_window_picker",
+          ["<cr>"] = "open_with_window_picker",
+          ["<tab>"] = { "toggle_preview", config = { use_float = true } },
+          ["<esc>"] = "close_window",
+        },
+      },
+    },
+  },
 }
