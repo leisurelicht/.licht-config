9dc3c3d22eb32f1feacbba573c817ad252e4bedd
暮晨 <leisurelicht@gmail.com>
2023-10-20 15:53:36 +0800

    refactor: neo-tree config

--- vi/nvim/lua/plugins/editor/neo-tree.lua
+++ vi/nvim/lua/plugins/editor/neo-tree.lua
@@ -22,32 +22,18 @@ return {
       close_if_last_window = true,
       source_selector = {
         winbar = true,
-        statusline = true,
         content_layout = "center",
-        sources = {
-          { source = "filesystem", display_name = " File" },
-          { source = "git_status", display_name = "󰊢 Git" },
-          { source = "buffers", display_name = "󰈙 Bufs" },
-        },
       },
       window = {
         mappings = {
           ["<space>"] = "none",
           ["S"] = "none",
-          ["m"] = "none",
+          -- ["m"] = "none",
           ["o"] = "split_with_window_picker",
           ["s"] = "vsplit_with_window_picker",
           ["<cr>"] = "open_with_window_picker",
           ["<tab>"] = { "toggle_preview", config = { use_float = true } },
           ["<esc>"] = "close_window",
-        },
-      },
-      event_handlers = {
-        {
-          event = "neo_tree_buffer_enter",
-          handler = function(_)
-            vim.opt_local.signcolumn = "auto"
-          end,
         },
       },
     },
