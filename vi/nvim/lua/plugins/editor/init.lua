1c813b07fe9ef85acfc435537e7372dcca73f84f
暮晨 <leisurelicht@gmail.com>
2024-05-22 11:18:31 +0800

    feat: delete bufremove config

--- vi/nvim/lua/plugins/editor/init.lua
+++ vi/nvim/lua/plugins/editor/init.lua
@@ -37,24 +37,6 @@ return {
     opts = { modes = { char = { jump_labels = true } } },
   },
   {
-    "echasnovski/mini.bufremove",
-    optional = true,
-    event = { "BufRead", "BufNewFile" },
-    keys = {
-      { "<leader>bd", false },
-      { "<leader>bD", false },
-    },
-    config = function()
-      map.set("n", "<leader>bd", function()
-        require("mini.bufremove").delete(0, false)
-      end, { desc = "Delete buffer" })
-
-      map.set("n", "<leader>bD", function()
-        require("mini.bufremove").delete(0, true)
-      end, { desc = "Delete Buffer (Force)" })
-    end,
-  },
-  {
     "mrjones2014/smart-splits.nvim",
     event = { "BufRead", "BufNewFile" },
     opts = {
