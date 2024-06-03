315024d78a761c7180bf0559f26597ab9cf70f23
暮晨 <leisurelicht@gmail.com>
2024-06-03 15:08:36 +0800

    feat: 🎸 manaul handle some extra plugins

--- vi/nvim/lua/plugins/plugins.lua
+++ vi/nvim/lua/plugins/plugins.lua
@@ -1,8 +1,18 @@
 return {
   { import = "lazyvim.plugins.extras.ui.treesitter-context" },
-  { import = "lazyvim.plugins.extras.util.project" },
+  { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+
   { import = "lazyvim.plugins.extras.editor.navic" },
+  { import = "lazyvim.plugins.extras.editor.dial" },
+
+  { import = "lazyvim.plugins.extras.coding.luasnip" },
+  { import = "lazyvim.plugins.extras.coding.mini-comment" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+
+  { import = "lazyvim.plugins.extras.util.project" },
+
   { import = "lazyvim.plugins.extras.lsp.none-ls"},
+
 
 
   { "wakatime/vim-wakatime", enabled = true },
