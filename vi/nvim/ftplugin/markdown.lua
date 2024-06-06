6237261250e92b18cbc029c4fc1ea973d1df7315
暮晨 <leisurelicht@gmail.com>
2024-06-06 14:25:47 +0800

    feat: 🎸 move nvim to a indendent repository

--- vi/nvim/ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
-vim.opt_local.tabstop = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.shiftwidth = 2
-
-vim.opt_local.list = true
-vim.opt_local.listchars:append("space:·")
-vim.opt_local.listchars:append("eol:↴")
